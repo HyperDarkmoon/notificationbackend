cebc55dfc27ecbacae5e39d2cb68639b8e396bf0
Chedly A Badri <119693735+HyperDarkmoon@users.noreply.github.com>
2025-07-09 15:35:53 +0100

    Initial commit

---
+++ src/main/java/org/hyper/notificationbackend/NotificationbackendApplication.java
@@ -0,0 +1,13 @@
+package org.hyper.notificationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hyper/notificationbackend/NotificationbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyper.notificationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
